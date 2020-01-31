1dfa84610709b3864957057aeceeb8386c346b46
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2020-01-31 22:37:10 +0400

    M

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Express = require('express');
+const express = require('express');
 const BodyParser = require('body-parser');
 const { Botact } = require('botact');
 
@@ -6,7 +6,7 @@ const { Botact } = require('botact');
 const Server = express ();
 const Bot = new Botact({
     token: '70306c7181f649b750ceacfe99520e60a8c691b1b4d920fe746d5362de5615aa245c674becf42ca038ae6',
-    Confirmation: 'cec5c527'
+    confirmation: 'cec5c527'
 });
 
 Bot.on(function(ctx) {
@@ -15,6 +15,6 @@ Bot.on(function(ctx) {
 
 Server.use(BodyParser.json());
 
-Server.post('/', bot.listen);   //путь к серверу, функция бота
+Server.post('/', Bot.listen);   //путь к серверу, функция бота
 
-Server.listen(80); //порт на котором сервер запущен
+Server.listen(80, (console.log("Server is working on port: 80"))); //порт на котором сервер запущен
