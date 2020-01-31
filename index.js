277b876586bcaf0049af7e9c164435009b3a2116
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2020-01-31 23:05:33 +0400

    1

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ server.use(BodyParser.json());
 
 server.post('/', bot.listen);
 
+server.get("/", (_,res) => res.send("hello world!"));
+
 server.listen(80, () => console.log("Server is working on port: 80"));
