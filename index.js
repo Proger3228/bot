eeb52caa7e77ed3b1bb3171d77ce0ec53aba4dc4
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2020-01-31 23:01:15 +0400

    m

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const BodyParser = require('body-parser');
-const { Botact } = require('botact');
+const express = require ('express');
+const BodyParser = require ('body-parser');
+const { Botact } = require ('botact');
 
 
-const Server = express ();
-const Bot = new Botact({
-    token: '70306c7181f649b750ceacfe99520e60a8c691b1b4d920fe746d5362de5615aa245c674becf42ca038ae6',
-    confirmation: 'cec5c527'
+const server = express ();
+const bot = new Botact({
+    token: 'e5100c829eadf1102b0ecafcc47e0ca90f16f8b3f8c7193dc3899be1d7f0f7a938437bfcb8f79bd6f982b',
+    confirmation: 'c03cd5c9'
 });
 
-Bot.on(function(ctx) {
-    Console.log(ctx.body)             //учим бота, тут писать, что он будет делать
-});
+bot.on(ctx => console.log(ctx));
 
-Server.use(BodyParser.json());
+server.use(BodyParser.json());
 
-Server.post('/', Bot.listen);   //путь к серверу, функция бота
+server.post('/', bot.listen);
 
-Server.listen(80, (console.log("Server is working on port: 80"))); //порт на котором сервер запущен
+server.listen(80, () => console.log("Server is working on port: 80"));
