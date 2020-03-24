f4f57ed18cdc1482591367c4dd07b4ae7fb5b2fd
IdkWhyy <damir_221003@mail.ru>
2020-03-24 16:14:01 +0400

    deleteline

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ bot.command(/дз/i, (ctx) => {
 
 bot.command(/helo/, (ctx) => console.log(ctx));
 
-
 bot.command(/hyd/, (ctx) => ctx.reply("ALL GOOD"));
 
 bot.command(/bye/, ctx => ctx.reply("bye bye"));
