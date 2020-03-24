ba5cdc90711cc91ec5e5dda77f821a68cdb8ea2e
Proger3228 <damir221003@gmail.com>
2020-03-24 15:08:10 +0400

    changed message

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.command(/расписание/i, (ctx) => {
     }
 });
 bot.command(/дз/i, (ctx) => {
-    ctx.reply("Дз не будет (бот принял ислам)")
+    ctx.reply("Дз будет но позже")
 });
 
 bot.startPolling();
